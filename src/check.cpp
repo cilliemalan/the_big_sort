f2f886389a1d5b4a4a0f20bb484b3d18b561653e
Cillié Malan <cilliemalan@gmail.com>
2018-08-08 13:01:54 +0200

    initial check program

---
+++ src/check.cpp
@@ -0,0 +1,31 @@
+/*
+    See LICENSE
+
+    This utility generates random stuff.
+*/
+
+
+#include "common.hpp"
+
+static void print_usage()
+{
+    std::cerr << R"(
+checks if a file is sorted.
+
+    Usage: check <unsorted file> <sorted file>
+
+The program will print status to stderr and return 0 if the file is stable sorted. The
+unsorted file is needed in order to check if the sort was stable.
+
+)";
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        print_usage();
+    }
+
+    return 0;
+}

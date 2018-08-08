c6e1a371970c07943700ee2ed3d82d73dea32054
Cillié Malan <cilliemalan@gmail.com>
2018-08-08 14:00:01 +0200

    Updated cloud init script

--- cloud_init.sh
+++ cloud_init.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 # This is the init script to mount and configure the nvme drives on an m5d.4xlarge server
 
+# make /tmp be a ram filesystem
+cat <<EOF >> /etc/fstab
+tmpfs   /tmp       tmpfs   defaults,noatime,mode=1777,size=1g    0  0
+tmpfs   /var/spool tmpfs   defaults,noatime,mode=1777,size=10m   0  0
+tmpfs   /var/tmp   tmpfs   defaults,noatime,mode=1777,size=10m   0  0
+EOF
+
+mount -a
+
 # we're going to need some deps
 apt update
-apt install docker.io build-essential
+apt dist-upgrade -y
+apt install -y docker.io build-essential
 
 mkdir -p /tmp/build
 cd /tmp/build
 
 git clone https://github.com/cilliemalan/the_big_sort
-cd the_big_sort
+cd the_big_sort/src
 make
 
 cp generate /usr/bin/
 
+cd /
+rm -rf /tmp/build
 
-# create and mount NVME drives
 
-function initialize(drive,mountpoint) {
+# create and mount NVME partitions and filesystems
+
+function initialize {
+local drive=$1
+local mountpoint=$2
+local part=${drive}p1
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${drive}
   o # clear the in memory partition table
   n # new partition
@@ -29,10 +45,18 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${drive}
   q # quit just in case
 EOF
 
-mkfs.ext4 ${drive}p1
+sleep 1
+
+mkfs.ext4 ${part}
+local uuid=$(blkid ${part} | sed -r 's/^.+UUID="(.+?)" TYPE.+/\1/')
+echo "UUID=${uuid}  ${mountpoint}     ext4    noatime,discard,errors=remount-ro  0  0" \
+    >> /etc/fstab
 mkdir -p ${mountpoint}
-mount ${drive}p1 ${mountpoint}
+
+tune2fs -O ^has_journal ${part}
+tune2fs -o discard ${part}
 }
 
 initialize /dev/nvme1n1 /src
 initialize /dev/nvme2n1 /dst
+mount -a

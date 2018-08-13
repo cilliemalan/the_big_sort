41ccbf40af7b8f58379c74566b8cf894df05b1e4
Cillié Malan <cilliemalan@gmail.com>
2018-08-13 15:12:10 +0200

    Renamed check -> check-sorted

--- src/check-sorted.cpp
+++ src/check-sorted.cpp
@@ -12,7 +12,7 @@ static void print_usage()
     std::cerr << R"(
 checks if a file is sorted.
 
-    Usage: check <unsorted file> <sorted file>
+    Usage: check-sorted <unsorted file> <sorted file>
 
 The program will print status to stderr and return 0 if the file is stable sorted. The
 unsorted file is needed in order to check if the sort was stable.

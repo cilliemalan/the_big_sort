09edbd45ec627245bfe4f376f99d0b0c3ddb5992
Cillié Malan <cilliemalan@gmail.com>
2018-08-20 16:50:59 +0200

    Updated purger to be slightly more efficient

--- src/purger.cpp
+++ src/purger.cpp
@@ -2,30 +2,35 @@
 #include <vector>
 
 using namespace std;
-constexpr size_t increment = 4*1024*1024;
+typedef intptr_t elem_t;
+constexpr size_t increment = 1024 * 1024;
+constexpr size_t gigabyte = 1024 * 1024 * 1024;
 
-int main(int, char**)
+int main(int, char **)
 {
     cout << "purging all shared and cached memory...\n";
     size_t total = 0;
     {
-        vector<void*> blocks;
+        vector<elem_t *> blocks;
         try
         {
-            for(;;)
+            for (;;)
             {
-                auto block = new char[increment];
-                memset(block, 0, increment);
+                auto block = new elem_t[increment];
+                for (size_t i = 0; i < increment; i++)
+                    block[i] = 0;
                 blocks.push_back(block);
-                total += increment;
+                total += increment * sizeof(elem_t);
+                if ((total % gigabyte) == 0)
+                {
+                    cout << "\rpurged " << (total / gigabyte) << " gigabytes of memory";
+                }
             }
         }
-        catch(...) {}
-
-        std::cout << blocks.size() << "\n";
-        std::cout << "press any key...\n";
-        getc(stdin);
+        catch (...)
+        {
+        }
     }
 
-    cout << "purged " << (total/1024/1024/1024) << " gigabytes of memory\n";
+    cout << "\rpurged " << (total / gigabyte) << " gigabytes of memory\n";
 }

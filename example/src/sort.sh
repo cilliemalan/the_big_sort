e9f811c89eeea0b160af7ac3adf1c62a0722b9ae
Cillié Malan <cilliemalan@gmail.com>
2018-08-21 19:33:00 +0200

    Updated sort image

--- example/src/sort.sh
+++ example/src/sort.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/sh
 # This example script will sort using sort(1). YOU CANNOT USE THIS. obviously.
 # This is just an example of how your dockerfile must work
 
+sort -f -o $2 $1

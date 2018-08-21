fd422eed4cf57889a14d67fa636994ed475e3fe8
Cillié Malan <cilliemalan@gmail.com>
2018-08-21 19:57:24 +0200

    Line endings

--- example/src/sort.sh
+++ example/src/sort.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/sh
+#!/bin/sh
 # This example script will sort using sort(1). YOU CANNOT USE THIS. obviously.
 # This is just an example of how your dockerfile must work
-
-sort -f -o $2 $1
+echo sort -f -o $2 $1
+/usr/bin/sort -f -o $2 $1
+echo done

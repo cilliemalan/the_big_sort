f0a78462e1b73370e1e60ccaa16977f286fa9a63
Cillié Malan <cilliemalan@gmail.com>
2018-08-21 20:18:14 +0200

    locale c for sorter example

--- example/src/sort.sh
+++ example/src/sort.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # This example script will sort using sort(1). YOU CANNOT USE THIS. obviously.
 # This is just an example of how your dockerfile must work
 echo sort -f -o $2 $1
-/usr/bin/sort -f -o $2 $1
+LOCALE=C /usr/bin/sort -f -o $2 $1
 echo done

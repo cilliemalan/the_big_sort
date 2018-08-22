538aef6796b249f89c5ab02ed4f28aeffd0f378c
Cillié Malan <cilliemalan@gmail.com>
2018-08-22 21:16:25 +0200

    implemented linux mapped filing

--- src/mappedfile.cpp
+++ src/mappedfile.cpp
@@ -4,16 +4,44 @@
 mapped_file::mapped_file(std::string filename, bool readonly)
 {
 #if defined(__unix__)
+
+    // open file
+    file_handle = ::open(filename.c_str(),
+        readonly ? O_RDONLY : O_RDWR);
+
+    if (!file_handle || file_handle == INVALID_HANDLE_VALUE)
+    {
+        throw std::runtime_error("could not open file");
+    }
+
+    fseek(file_handle, 0L, SEEK_END);
+    filesize = ftell(file_handle);
+    rewind(file_handle);
+
+    // memory map
+    pointer = static_cast<char*>(mmap(
+        0,
+        filesize,
+        readonly ? PROT_READ : PROT_WRITE,
+        MAP_SHARED,
+        file_handle,
+        0));
+
+    if(!pointer || pointer == MAP_FAILED)
+    {
+        close(file_handle);
+        throw std::runtime_error("could not open file");
+    }
 #elif defined(WIN32)
 
     // open the file
     file_handle = CreateFileA(filename.c_str(),
-                              readonly ? GENERIC_READ : GENERIC_READ | GENERIC_WRITE,
-                              FILE_SHARE_READ | FILE_SHARE_WRITE,
-                              0,
-                              OPEN_EXISTING,
-                              FILE_ATTRIBUTE_NORMAL,
-                              0);
+        readonly ? GENERIC_READ : GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        0,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        0);
     if (!file_handle || file_handle == INVALID_HANDLE_VALUE)
     {
         throw std::runtime_error("could not open file");
@@ -58,9 +86,18 @@ mapped_file::mapped_file(std::string filename, bool readonly)
 mapped_file::~mapped_file()
 {
 #if defined(__unix__)
+    if(filesize && pointer) munmap(filesize);
+    if(file_handle) close(file_handle);
 #elif defined(WIN32)
-    CloseHandle(mapping_handle);
-    CloseHandle(file_handle);
-    pointer = nullptr;
+    if(mapping_handle && mapping_handle != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(mapping_handle);
+    }
+    if(file_handle && file_handle != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(file_handle);
+    }
 #endif
+
+    pointer = nullptr;
 }

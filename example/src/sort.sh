cb6e854998834952fef5aac1cc4f4b9963472abb
Cillié Malan <cilliemalan@gmail.com>
2018-08-24 00:43:45 +0200

    updated sort example blurb output

--- example/src/sort.sh
+++ example/src/sort.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This example script will sort using sort(1). YOU CANNOT USE THIS. obviously.
 # This is just an example of how your dockerfile must work
-echo sort -f -o $2 $1
+echo LC_ALL=C /usr/bin/sort --parallel=20 -S 45G -T $(dirname "$2") -f -o $2 $1
 LC_ALL=C /usr/bin/sort --parallel=20 -S 45G -T $(dirname "$2") -f -o $2 $1
 echo done

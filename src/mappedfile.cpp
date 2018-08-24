aba792bdfcb381cb0e83b788f08979366fa192d3
Cillié Malan <cilliemalan@gmail.com>
2018-08-24 10:37:54 +0200

    Memory mapped file now truncates the file

--- src/mappedfile.cpp
+++ src/mappedfile.cpp
@@ -15,17 +15,28 @@ mapped_file::mapped_file(std::string filename, bool readonly, size_t size)
         throw std::runtime_error("could not open file");
     }
 
-    if(filesize == 0)
+    if (filesize == 0)
     {
         // get file size
         struct stat filestats;
         auto statresult = fstat(file_handle, &filestats);
-        if(statresult != 0)
+        if (statresult != 0)
         {
             close(file_handle);
             throw std::runtime_error("could not stat file");
         }
         filesize = filestats.st_size;
+    }
+    else
+    {
+        if (!readonly)
+        {
+            if (truncate(file_handle, filesize))
+            {
+                close(file_handle);
+                throw std::runtime_error("could not truncate file");
+            }
+        }
     }
 
     // memory map
@@ -37,7 +48,7 @@ mapped_file::mapped_file(std::string filename, bool readonly, size_t size)
         file_handle,
         0));
 
-    if(!pointer || pointer == MAP_FAILED)
+    if (!pointer || pointer == MAP_FAILED)
     {
         close(file_handle);
         throw std::runtime_error("could not open file");
@@ -68,6 +79,18 @@ mapped_file::mapped_file(std::string filename, bool readonly, size_t size)
     {
         hi_size = static_cast<DWORD>(filesize >> 32);
         lo_size = static_cast<DWORD>(filesize & 0xFFFFFFFF);
+
+        if (!readonly)
+        {
+            LONG lhi_size = *reinterpret_cast<PLONG>(&hi_size);
+            LONG llo_size = *reinterpret_cast<PLONG>(&lo_size);
+            SetFilePointer(file_handle,
+                llo_size,
+                &lhi_size,
+                FILE_BEGIN);
+            SetEndOfFile(file_handle);
+            SetFilePointer(file_handle, 0, nullptr, FILE_BEGIN);
+        }
     }
 
     // map it
@@ -99,19 +122,19 @@ mapped_file::mapped_file(std::string filename, bool readonly, size_t size)
         throw std::runtime_error("could not map file");
     }
 #endif
-}
+    }
 
 mapped_file::~mapped_file()
 {
 #if defined(__unix__)
-    if(filesize && pointer) munmap(pointer, filesize);
-    if(file_handle) close(file_handle);
+    if (filesize && pointer) munmap(pointer, filesize);
+    if (file_handle) close(file_handle);
 #elif defined(WIN32)
-    if(mapping_handle && mapping_handle != INVALID_HANDLE_VALUE)
+    if (mapping_handle && mapping_handle != INVALID_HANDLE_VALUE)
     {
         CloseHandle(mapping_handle);
     }
-    if(file_handle && file_handle != INVALID_HANDLE_VALUE)
+    if (file_handle && file_handle != INVALID_HANDLE_VALUE)
     {
         CloseHandle(file_handle);
     }
